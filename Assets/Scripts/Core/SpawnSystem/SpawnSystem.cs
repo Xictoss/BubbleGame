cacf6207f382df60f28226b125f4f9e2ba5b434f
ia7nael <7zekira@gmail.com>
2025-01-28 11:27:34 +0100

    animal list, button croquette machin

--- Assets/Scripts/Core/SpawnSystem/SpawnSystem.cs
+++ Assets/Scripts/Core/SpawnSystem/SpawnSystem.cs
@@ -1,25 +1,30 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
-using DG.Tweening.DOTween;
+using UnityEngine.UI;
 
 namespace BubbleGame.Core
 {
     public class SpawnSystem : MonoBehaviour
     {
+
+        [SerializeField] private Player player;
         [Header("Spawn Settings")]
-        public int croquettes = 50; //remove pour prendre la valeur présente dans le script du joueur 
+       // public int croquettes = 50; //remove pour prendre la valeur présente dans le script du joueur 
         //système de croquettes prcq jsuis une fraude j'ai pas compris qui fait quoi et où
+       
         public int coutEnCroquettes = 10; 
         [Header("Assignations")]
         [SerializeField] private List<GameObject> animals; //les trucs à spawn
         [SerializeField] private Transform spawnPoint;
+        [SerializeField] private Button button;
 
 
         public void SpawnObject()
         {
-            if (croquettes >= coutEnCroquettes)
+            if (player.Croquette >= coutEnCroquettes)
             {
-                croquettes -= coutEnCroquettes;//enlève les croquettes
+                player.Croquette -= coutEnCroquettes;//enlève les croquettes
 
                 Instantiate(animals[0], spawnPoint.position, spawnPoint.rotation);
 /* la poule c num 0 et ainsi de suite jusquà la chimere mais dcp jsp comment ça va s'organiser
@@ -29,8 +34,7 @@ namespace BubbleGame.Core
 
             else
             {
-                Debug.Log("Pas assez de croquettes mgl");
-                
+                button.transform.DOShakePosition(3, Vector3.right, 1000);
             }
         }
 
